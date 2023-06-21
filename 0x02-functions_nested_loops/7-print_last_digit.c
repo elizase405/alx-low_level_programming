8002dc70135f63f076167e1dbfbe0219656d128d
eliza <elizase405@gmail.com>
2023-06-21 08:14:22 -0700

    added c file to print last digit of a number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.c"
+
+/**
+ * print_last_digit - Entry point
+ * @n: number to be tested
+ * Description: prints last digit of a number
+ *
+ * Return: a digit
+ */
+
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
