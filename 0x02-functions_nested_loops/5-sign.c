ce3de511367ac0c03c956e2bf172cf9fc8eca55d
eliza <elizase405@gmail.com>
2023-06-21 07:53:52 -0700

    added c file to print sign of a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_sign - Entry point
+ * Description: prints 1 if n > 0, and 0 if n = 0 and -1 if n < 0
+ *
+ * Return: 0 or 1 or -1
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+		return (1);
+	else if (n = 0)
+		return (0);
+	else
+		return -1;
+}
