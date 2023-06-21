0658ac5908ff1e207e4dcf9021bb45dd3b97dab1
eliza <elizase405@gmail.com>
2023-06-21 08:05:26 -0700

    added c file to print absolute value of integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib>
+
+/**
+ * _abs - Entry point
+ * @int: integer value to be tested
+ * Description: computes absolute value of an integer
+ *
+ * Return: positive int
+ */
+
+int _abs(int n)
+{
+	int n = abs(n)
+	return n;
+}
