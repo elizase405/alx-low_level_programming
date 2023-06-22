b98584c8b3ffaa48e5e1acbd506334aba3c64e75
eliza <elizase405@gmail.com>
2023-06-22 13:08:43 -0700

    added c file to check if character is uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _isupper - checks if character is uppercase
+ * @c: character to be tested
+ *
+ * Return 1 or 0
+ */
+
+int _ispuuer(int c)
+{
+	if (isupper(c))
+		return 1;
+	return 0;
+}
