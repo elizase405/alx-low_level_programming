ed3acbb1ed14d139e1378e241a615be0a08146f2
eliza <elizase405@gmail.com>
2023-06-23 03:55:17 -0700

    fixed err with file 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,3 +1,4 @@
+#include "main.h"
 
 /**
  *print_numbers - a function that prints the numbers, from 0 to 9
@@ -11,6 +12,8 @@ void print_numbers(void)
 	int i;
 
 	for (i = 48; i <= 57; i++)
+	{
 		_putchar(i);
+	}
 	_putchar('\n');
 }
