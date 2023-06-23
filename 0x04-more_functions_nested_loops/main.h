3a81293042dedbcb7b2f73b2a5a327025e41a576
eliza <elizase405@gmail.com>
2023-06-23 03:38:28 -0700

    added new c files

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -6,6 +6,12 @@
 int _isupper(int c);
 int _putchar(char c);
 int _isdigit(int c);
+int mul(int num1, int num2);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(intn);
 
 #endif
 
