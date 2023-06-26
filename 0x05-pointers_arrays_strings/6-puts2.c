696f6085c34017c57ced066ec9523d246581d11d
eliza <elizase405@gmail.com>
2023-06-26 12:24:39 -0700

    fix wrong code

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,12 +9,13 @@
 
 void puts2(char *str)
 {
-	int i = 0;
+	int i = 0, j = 0;
 
-	while (str[i])
+	while (str[i] != '\0')
 	{
-		_putchar(str[i]);
-		i += 2;
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		i++;
 	}
 	_putchar('\n');
 }
