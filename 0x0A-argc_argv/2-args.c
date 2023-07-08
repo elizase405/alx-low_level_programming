6a509840df3d2be4ea110a93e01ff2785b068489
eliza <elizase405@gmail.com>
2023-07-08 04:13:21 -0700

    Add program to print given arguments

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Program that prints the arguments passed into it
+ * @argc: no of arguments
+ * @argv: array of string containing arguments as strings
+ *
+ * Return: int
+ */
+
+int main(int argc, char **argv)
+{
+	while(argc--)
+		printf("%s\n", *argv++);
+	return (0);
+}
