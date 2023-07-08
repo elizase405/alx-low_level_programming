85c8967eab8a7ab9d50ce4c9ab2b936d01281452
eliza <elizase405@gmail.com>
2023-07-08 09:12:11 -0700

    Fix code

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,16 +9,21 @@
  * Return: int
  */
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	(void) argc /*ignore argc*/
+	int num1, num2, prod;
+
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	int num1 = atoi(argv[1]), num2 = atoi(argv[2]);
 
-	printf("%d\n", num1 * num2);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	prod = num1 * num2;
+
+	printf("%d\n", prod);
+
 	return (0);
 }
