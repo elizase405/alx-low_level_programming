03d91d6d9683bc8d2e81b66cad591bd12354847b
eliza <elizase405@gmail.com>
2023-07-11 04:37:06 -0700

    Add function that points to new space in memory containing parameter value

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * _strdup - function that returns array of given parameter
+ * Description: returns a pointer to a new string which is a
+ * duplicate of the string str. Memory for the new string is
+ * obtained with malloc, and can be freed with free.
+ * @str: array to be duplicated
+ *
+ * Return: char *
+ */
+
+char *_strdup(char *str)
+{
+	int j, i = 0;
+
+	while (*(str + i) != 0)
+		i++;
+
+	char *copy = (char *)malloc(sizeof(char) * (i + 1));
+
+	if (copy == NULL || str == NULL)
+		return (NULL);
+
+	for (j = 0; j <= i; j++)
+		*(copy + j) = str[j];
+
+	return (copy);
+}
