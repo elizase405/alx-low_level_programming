ca73073ba79e3ae662861bb0110aa2d6d8ca37e1
eliza <elizase405@gmail.com>
2023-07-11 06:50:17 -0700

    Added test case for Null array

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,8 +18,10 @@ char *str_concat(char *s1, char *s2)
 	int i, z, j = 0, k = 0;
 	char *concat;
 
-	if (s1 == NULL || s2 == NULL)
-		return ("");
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
 	while (s1[j] != 0)
 		j++;
