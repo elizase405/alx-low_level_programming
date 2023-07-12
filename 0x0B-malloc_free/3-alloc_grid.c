c771d16b0aeaba4f0f7cf0cefeb2e4268090213c
eliza <elizase405@gmail.com>
2023-07-12 05:44:05 -0700

    Add function to print 2d array

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+
+/**
+ * row_func - function to create an array filled with 0
+ * @width: no of 0 in array
+ *
+ * Return: int *
+ */
+
+int *row_func(int width)
+{
+	int i, *arr;
+
+	arr = malloc(sizeof(int) * width);
+
+	if (arr == NULL)
+		return NULL;
+	
+	for (i = 0; i < width; i++)
+		arr[i] = 0;
+
+	return (arr);
+}
+/**
+ * alloc_grid - function to create 2d array
+ * @width: no of rows of array
+ * @height: no of columns of array
+ *
+ * Return: pointer to 2d array)
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int i = 0, **array_of_array;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	array_of_array = malloc(sizeof(int) * (width * height));
+
+	if (array_of_array == NULL)
+		return (NULL);
+
+	while (i < height)
+	{
+		array_of_array[i] = row_func(width);
+		i++;
+	}
+
+	return (array_of_array);
+}
