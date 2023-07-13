6a4d9168cdf6d63512deba2ed5d2a37fc72f8d51
eliza <elizase405@gmail.com>
2023-07-13 08:14:14 -0700

    Add function to concatenate 2 string into new pointer using malloc

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,50 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * string_nconcat - function that concatenates 2 strings
+ * @n: length of string s2 to add to pointer
+ * @s1: string to be added to new pointer
+ * @s2: string to be added to new pointer from index 0 to n.
+ *
+ * Return: pointer to new array
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int i = 0, j = 0, lens1, lens2, z;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[i] != 0)
+		i++;
+	while (*(s2 + j) != '\0')
+		j++;
+
+	lens1 = i;
+
+	(n >= j) ? (lens2 = j) : (lens2 = n);
+
+	char *concat = malloc(sizeof(s1[i]) * (lens1 + lens2 + 1));
+
+	if (concat == NULL)
+		return (NULL);
+
+	for (z = 0; z < lens1; z++)
+		concat[z] = s1[z];
+
+	int newlen = lens1;
+
+	for (z = 0; z < lens2; z++)
+	{
+		concat[newlen] = s2[z];
+		newlen++;
+	}
+
+	concat[lens1 + lens2 + 1] = '\0';
+
+	return (concat);
+}
