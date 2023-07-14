e9949e553b7c32dbc071b4aaab5e8b3c9640d9ba
eliza <elizase405@gmail.com>
2023-07-14 14:13:45 -0700

    Add more test cases

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -15,12 +15,19 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	char *mal, *p;
 	unsigned int i = 0;
+
+	if (ptr == NULL)
+		return ((void *)malloc(new_size));
+
 	p = ((char *)ptr);
 
 	if (new_size == old_size)
 		return (ptr);
 
 	mal = malloc(sizeof(char) * new_size);
+
+	if (mal == NULL)
+		return (NULL);
 
 	while (i < old_size)
 	{
