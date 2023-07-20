a81335e5661f579ff7e48ed7305406f8f1a7fe73
eliza <elizase405@gmail.com>
2023-07-20 06:58:12 -0700

    Add function to search for an integer

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+/**
+ * int_index - function that searches for an integer
+ * @array: array to be looped through
+ * @size: length of array
+ * @cmp: pointer to the function to be used to compare values
+ *
+ * Return: int
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i, result;
+
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		result = cmp(array[i]);
+
+		if (result >= 1)
+			return (i);
+	}
+	return (-1);
+}
