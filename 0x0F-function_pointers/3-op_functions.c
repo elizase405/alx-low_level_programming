31b1a86b3fc571be17370ff656885c0cd532759f
eliza <elizase405@gmail.com>
2023-07-20 08:56:28 -0700

    Tried my best

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,60 @@
+/**
+ * op_add - function to sum 2 values
+ * @a: first int value
+ * @b: second int value
+ *
+ * Return: int/result
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - function to subtract from 2 values
+ * @a: first int value
+ * @b: second int value
+ *
+ * Return: int/result
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - function to multiply 2 values
+ * @a: first int value
+ * @b: second int value
+ *
+ * Return: int/result
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - function to divide 2 integers
+ * @a: first int value
+ * @b: second int value
+ *
+ * Return: int/result
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - function to return the remainder of the division of 2 values
+ * @a: first int value
+ * @b: second int value
+ *
+ * Return: int/result
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
