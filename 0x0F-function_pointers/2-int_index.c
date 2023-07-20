52e1e7cd6d478108e9ae14a90f4fe0070f9fa339
eliza <elizase405@gmail.com>
2023-07-20 07:05:24 -0700

    Add more test case

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 /**
  * int_index - function that searches for an integer
  * @array: array to be looped through
@@ -11,7 +12,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i, result;
 
-	if (size <= 0)
+	if (size <= 0 || array == NULL || cmp == NULL)
 		return (-1);
 
 	for (i = 0; i < size; i++)
