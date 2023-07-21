ac3e1a7f80f59aefcc365f008590726adc6fa384
eliza <elizase405@gmail.com>
2023-07-21 13:27:19 -0700

    add test case for if separator is NULL

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -19,7 +19,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	while (i < n)
 	{
-		if (i == (n - 1))
+		if (i == (n - 1) || separator == NULL)
 			printf("%d", va_arg(ap, int));
 		else
 			printf("%d%s", va_arg(ap, int), separator);
