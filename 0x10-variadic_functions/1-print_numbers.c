2f3b24566a07f91a6ad494bb84c37694c50883a1
eliza <elizase405@gmail.com>
2023-07-21 13:14:56 -0700

    Add function to print numbers in parameter

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_numbers - prints numbers given as parameters
+ * @separator: string to be printed between numbers
+ * @n: number of integers passed to function
+ * @...: a variable number of parameter to be printed
+ *
+ * Return: void
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i = 0;
+
+	va_start(ap, n);
+
+	while (i < n)
+	{
+		if (i == (n - 1))
+			printf("%d", va_arg(ap, int));
+		else
+			printf("%d%s", va_arg(ap, int), separator);
+		i++;
+	}
+
+	printf("\n");
+}
