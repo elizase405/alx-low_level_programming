5262b45857b2542b63ff4101e248c9e3acf2bf95
eliza <elizase405@gmail.com>
2023-08-03 19:39:53 -0700

    Remove unnecessary code

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -27,25 +27,19 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (*head == NULL || idx == 0)
 	{
-		new_node->next = NULL;
+		new_node->next = temp;
 		*head = new_node;
 		return (new_node);
 	}
-	else
+	for (i = 0; i < (idx - 1); i++)
 	{
-		temp = *head;
-		for (i = 0; i < (idx - 1); i++)
-		{
-			printf("Looping\t");
-			temp = temp->next;
-			if (temp->next == NULL)
-				return (NULL);
-		}
-		printf("Inserting\t");
-		new_node->next = temp->next;
-		temp->next = new_node;
+		temp = temp->next;
+		if (temp->next == NULL)
+			return (NULL);
+	}
+	new_node->next = temp->next;
+	temp->next = new_node;
 	}
 
-	printf("Inserted\n");
 	return (temp);
 }
