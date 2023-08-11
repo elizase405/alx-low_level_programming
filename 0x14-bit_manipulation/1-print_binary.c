e7fce8f0da06af81eb1e5b7f6c8553858903208b
eliza <elizase405@gmail.com>
2023-08-11 09:50:18 -0700

    Add function to convert from decimal to binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,13 @@
+/**
+ * print_binary - converts decimal number/base 10 to binary/base 2
+ * @n: number to be turned to binary
+ *
+ * Return: void
+ */
+
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) + '0');
+}
