eb5532301abfac1d63a2f940aadd21ffb91655ab
Elizabeth <elizase405@gmail.com>
2023-12-29 23:54:55 -0800

    pycode error fix

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -12,7 +12,7 @@ def island_perimeter(grid):
     Args: grid
     """
 
-    widths = [] # stores the columns where a 1 appear
+    widths = []     # stores the columns where a 1 appear
     length = 0
 
     for row in grid:
@@ -20,18 +20,22 @@ def island_perimeter(grid):
             length += 1     # so if there is one 1 in a row, length increase
         column = 0
         for val in row:
-            column += 1     # for each iteration, we move to a new column, column tells us what column we are in
+            column += 1     # for each iteration, we move to a new column,
+# column tells us what column we are in
             if val == 1:
-                widths.append(column) # if a value is 1, we append the column number of the value
+                widths.append(column)       # if a value is 1, we append
+# the column number of the value
 
-    #print(grid)
-    #print(f"columns with a 1: {widths}")
+    # print(grid)
+    # print(f"columns with a 1: {widths}")
 
-    widths = set(widths)    # since we only need one 1 per column, we make it a set to ensure unique columns
-    width = len(widths)     # we get the length of the set, this will be our width
+    widths = set(widths)    # since we only need one 1 per column,
+# we make it a set to ensure unique columns
+    width = len(widths)     # we get the length of the set,
+# this will be our width
 
-    #print(f"rows with a 1: {length}\nunique columns with a 1: {widths}")
-    #print(f"length: {length} width: {width}")
+    # print(f"rows with a 1: {length}\nunique columns with a 1: {widths}")
+    # print(f"length: {length} width: {width}")
 
     if length > 0:
         return 2 * (length + width)
--- 0x1C-makefiles/5-main.py
+++ 0x1C-makefiles/5-main.py
@@ -22,3 +22,12 @@ if __name__ == "__main__":
             [0, 0, 0, 1, 1, 1]
     ]
     print(f"perimeter: {island_perimeter(grid)}")
+    print()
+    grid = [
+            [0, 1, 0, 0, 0, 1],
+            [1, 1, 0, 0, 0, 1],
+            [1, 1, 0, 1, 1, 1],
+            [0, 1, 1, 1, 0, 0],
+            [0, 0, 1, 1, 0, 0]
+    ]
+    print(f"perimeter: {island_perimeter(grid)}")
