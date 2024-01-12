ec28afaab80e12a8038128c93d119e69fc6b3903
Elizabeth <elizase405@gmail.com>
2024-01-12 09:50:38 -0800

    chore: betty errors

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -11,15 +11,15 @@
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new = NULL;
-	
+
 	new = malloc(sizeof(dlistint_t));
 
 	if (!new)
 		return (NULL);
 
 	new->n = n;
-        new->prev = NULL;
-        new->next = *head;
+	new->prev = NULL;
+	new->next = *head;
 
 	if (*head)
 		(*head)->prev = new;
