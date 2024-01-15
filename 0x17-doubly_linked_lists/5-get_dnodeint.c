ac4de8cb04af38e26ae811ef94663f8fee4a47d0
Elizabeth <elizase405@gmail.com>
2024-01-15 04:50:49 -0800

    fix: fix issue of segmentation error when index out of range

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -12,12 +12,12 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
 
-	while (i < index)
+	while (head && i < index)
 	{
 		head = head->next;
 		i++;
 	}
-	if (!head)
+	if (head == NULL)
 		return (NULL);
 
 	return (head);
